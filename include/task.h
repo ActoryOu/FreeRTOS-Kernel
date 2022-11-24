7f623365f65d5b255b87d1d44a3e991cda53fb0b
ActoryOu <ousc@amazon.com>
2022-11-24 03:28:13 +0000

    Fix MISRA_C_DIR_20_7 violations

--- include/task.h
+++ include/task.h
@@ -277,7 +277,7 @@ typedef enum
 #define taskSCHEDULER_RUNNING        ( ( BaseType_t ) 2 )
 
 /* Checks if core ID is valid. */
-#define taskVALID_CORE_ID( xCoreID )    ( ( 0 <= xCoreID ) && ( xCoreID < ( BaseType_t ) configNUM_CORES ) )
+#define taskVALID_CORE_ID( xCoreID )    ( ( 0 <= ( xCoreID ) ) && ( ( xCoreID ) < ( BaseType_t ) configNUM_CORES ) )
 
 /*-----------------------------------------------------------
 * TASK CREATION API
