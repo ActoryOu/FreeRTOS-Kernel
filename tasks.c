224f4eaccbf33f44cc6a5f9346c21af45d15149b
ActoryOu <ousc@amazon.com>
2022-11-28 08:27:20 +0000

    Fix MISRA C 2012 Rule 10.1.

--- tasks.c
+++ tasks.c
@@ -1468,6 +1468,10 @@ static void prvInitialiseNewTask( TaskFunction_t pxTaskCode,
     #if ( portSTACK_GROWTH < 0 )
     {
         pxTopOfStack = &( pxNewTCB->pxStack[ ulStackDepth - ( uint32_t ) 1 ] );
+
+        /* Avoiding casts between pointers and integers is not practical. 
+         * Size differences accounted for using portPOINTER_SIZE_TYPE type. Checked by assert(). */
+        /* coverity[misra_c_2012_rule_10_1_violation] */
         pxTopOfStack = ( StackType_t * ) ( ( ( portPOINTER_SIZE_TYPE ) pxTopOfStack ) & ( ~( ( portPOINTER_SIZE_TYPE ) portBYTE_ALIGNMENT_MASK ) ) ); /*lint !e923 !e9033 !e9078 MISRA exception.  Avoiding casts between pointers and integers is not practical.  Size differences accounted for using portPOINTER_SIZE_TYPE type.  Checked by assert(). */
 
         /* Check the alignment of the calculated top of stack is correct. */
